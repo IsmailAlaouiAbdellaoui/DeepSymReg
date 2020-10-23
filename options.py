c72455a70d90e89984e75ee10154291d15cf855a
Ismail Alaoui Abdellaoui <i.alaouiabdellaoui@student.maastrichtuniversity.nl>
2020-10-23 16:12:21 +0200

    update

--- options.py
+++ options.py
@@ -5,11 +5,11 @@ Created on Mon Oct 19 14:54:34 2020
 @author: smail
 """
 
-config = {"use_rescaled_MSE":False,
-          "epochs1":3,
-          "epochs2":3,
+config = {"use_rescaled_MSE":True,
+          "epochs1":100,
+          "epochs2":100,
           "threshold_value":0.01,
-          "a_L_0.5":5e-2,
+          "a_L_0.5":5e-3,
           "use_phase2":True,
           "use_thresholding_before_phase2":True,
           "lambda_reg":5e-3,
